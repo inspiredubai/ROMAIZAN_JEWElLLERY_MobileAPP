852c8213b239b1722b09ce1dc38bef03bd76e5c4
AFSAR RIAZ <AFSAR RIAZ@DESKTOP-QP7DBD2>
2025-10-22 15:38:26 +0500

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,12 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'romaizan-jeweillery',
   webDir: 'www',
-  server:{
-    androidScheme:'http',
-    cleartext:true,
-    allowNavigation:[
-      'http://103.74.54.207:8291/api/*'
-    ]
-  }};
+  server: {
+    androidScheme: 'http',
+    cleartext: true,
+    allowNavigation: ['http://194.233.95.37:8291/api/*'],
+  },
+};
 
 export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-       // apiRootURL: 'http://103.74.54.207:8092/api/',  // demo
-    apiRootURL: 'http://103.74.54.207:8291/api/',
-
+  // apiRootURL: 'http://103.74.54.207:8092/api/',  // demo
+  apiRootURL: 'http://194.233.95.37:8291/api/',
 };
